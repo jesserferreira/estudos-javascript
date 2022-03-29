026a3b4d42b5f1534acab1fd93efa20e4f9e04a5
Jessér Ramiro Ferreira <97418530+jesserferreira@users.noreply.github.com>
2022-03-28 23:58:06 -0300

    funções básico

---
+++ js-aula16/funcao-02.js
@@ -0,0 +1,8 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+  }
+  
+  console.log(soma(2,5))
+  console.log(soma(2,))
+  
+  // Os parâmetros " =0 " definem que quando um parâmetro não for informado, o mesmo será considerado zero
